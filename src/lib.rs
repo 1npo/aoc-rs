dcf84c49f1c3bf99ee42af8d289be0b44eff65ab
1npo <nickpeteromalley@gmail.com>
2024-12-01 23:16:11 -0500

    removed solver.rs; moved content to src/puzzles

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ pub use cli::{Cli, Method};
 mod web;
 pub use web::{get_input, post_puzzle_answer};
 
-mod solver;
-pub use solver::solve_puzzle;
-
 mod puzzles;
+pub use puzzles::solve_puzzle;
 pub use puzzles::year2024;
--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -1,6 +1,42 @@
 pub mod year2024;
 
 use std::collections::HashMap;
+use log::info;
+
+use crate::{Cli, Method, get_input, post_puzzle_answer};
+
+/// 1. Get the puzzle input for the given year and day (eg 2024, 1)
+/// 2. Run the function that solves the given part of the puzzle (ie 1 or 2)
+/// 3. Return the puzzle results using the given method:
+///     - Solve: Print the puzzle solution
+///     - Submit: Submit the puzzle solution to adventofcode.com and print a message
+///         indicating whether or not the solution was correct
+pub fn solve_puzzle(cli: Cli) {
+    let puzzle_input = match get_input(cli.year, cli.day) {
+        Ok(input) => {
+            info!("Got puzzle input for year {:?} day {:?} part {:?}",
+                cli.year,
+                cli.day,
+                cli.part);
+            input
+        },
+        Err(error) => panic!("Failed to get puzzle input! ({:?})", error)
+    };
+    
+    let solution = get_puzzle_solution(cli.year, cli.day, cli.part, puzzle_input);
+
+    match cli.method {
+        Method::Solve => info!("Solution: {solution:?}"),
+        Method::Submit => {
+            let solution_result = post_puzzle_answer(cli.year,
+                                                     cli.day,
+                                                     cli.part,
+                                                     solution);
+            info!("Submitted puzzle answer");
+            info!("{}", solution_result.unwrap());
+        }
+    }
+}
 
 /// This function lets us retrieve the function that solves the given part of a given
 /// day's puzzle. The key is a tuple of u8 integers that represent the year, day, and
